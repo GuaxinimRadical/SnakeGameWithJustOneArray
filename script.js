175961c8e7f40e9ec6418521790589ca376b96df
GuaxinimRadical <talisonf11@gmail.com>
2020-04-23 21:47:29 -0300

    Correção dos Bugs gerais e aplicação da possibilidade da cobra atravessar o mapa corretamente

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
    
     //Leitura Keyboard
     document.addEventListener('keydown', (event) => {
-        snake.modeMoviment(event.key)
+        snake.switchMoviment(event.key)
     });
 
     
 
 const display = document.getElementById('canvas')
 
-const widthDisplay = 7
+const widthDisplay = 8
 const heightDisplay = 7
 const pixelsDisplay = widthDisplay * heightDisplay
+var timerLoop
 
 const speedGame = 200
+
+const walls = false //Define se a cobra tentar atravessar a borda é considerado derrota
 
 let guy = []
 
 let indexFruit;
 
-let snake = {
+const snake = {
     indexHead: 0,
     length: 5,
     state: 'none',
-    modeMoviment(key){
-        if(key==='w')
+    switchMoviment(key){
+        if(key==='w' && this.state!=='down')
             this.state = 'up'
 
-        if(key==='s')
+        if(key==='s' && this.state!=='up')
             this.state = 'down'
 
-        if(key==='a')
+        if(key==='a' && this.state!=='right')
             this.state = 'left'
 
-        if(key==='d')
+        if(key==='d' && this.state!=='left')
             this.state = 'right'
-
     }
 }
 
@@ -45,7 +47,7 @@ function start(){
 
     startGame()
 
-    setInterval(loop,speedGame)
+    timerLoop = setInterval(loop,speedGame)
 
 }
 
@@ -53,7 +55,7 @@ function startGame(){
     const indexPixelInicio = Math.floor(pixelsDisplay/2)
 
     snake.indexHead = indexPixelInicio
-    guy[indexPixelInicio] = 5
+    guy[indexPixelInicio] = snake.length
     console.log(indexPixelInicio)
 
     renderDisplay()
@@ -70,20 +72,27 @@ function loop(){
 
 function eatFruit(){
     if(snake.state!=='none'){
-        
-        let nextIndex
 
+        if(walls==true && borderCollapse()!==false)
+            endGame()
+
+        let nextIndex
+        
         if(snake.state==='up'){
-            nextIndex = snake.indexHead - heightDisplay
+            nextIndex = nextIndex = borderCollapse()==='up' 
+                ? (snake.indexHead+((heightDisplay-1)*widthDisplay)) : snake.indexHead - widthDisplay
         }
         if(snake.state==='down'){
-            nextIndex = snake.indexHead + heightDisplay
+            nextIndex = nextIndex = borderCollapse()==='down' 
+                ? (snake.indexHead-((heightDisplay-1)*widthDisplay)) : snake.indexHead + widthDisplay
         }
         if(snake.state==='left'){
-            nextIndex = snake.indexHead - 1
+            nextIndex = borderCollapse()==='left' 
+                ? (snake.indexHead+widthDisplay)-1 : snake.indexHead - 1
         }
         if(snake.state==='right'){
-            nextIndex = snake.indexHead + 1
+            nextIndex = borderCollapse()==='right' 
+                ? (snake.indexHead-widthDisplay)+1 : snake.indexHead + 1
         }
 
         if(guy[nextIndex]<0){
@@ -92,31 +101,57 @@ function eatFruit(){
             guy[nextIndex] = snake.length
             fruitSpawn()
 
-        }else if(guy[nextIndex]===0){
+        } else if(guy[nextIndex]===0){
 
-            for(let i=0; i < pixelsDisplay; i++){
-                if( guy[i]>0){
-                    guy[i] -= 1
-                }
-            }
+            guy = guy.map( bloco => {
+                return bloco>0 ? bloco-=1 : bloco
+            })
             guy[nextIndex] = snake.length
-        }else if(guy[nextIndex]>0){
-            alert('YOU DIED!')
 
         } else{
-            console.log('SAIUUU!!')
-            //clearInterval()
-            restart()
+            endGame()
         }
 
         snake.indexHead = nextIndex
-        console.log(guy[nextIndex])
+        // console.log(guy[nextIndex])
     }
 }
 
+function borderCollapse(){
+    let wallRight = []
+    let wallLeft = []
+    let rowUp = []
+    let rowDown = []
+
+    for(let i =1; i<=heightDisplay; i++){ // for() começa em 1 e termina com o valor de heightDisplay
+        wallRight.push( (widthDisplay*i)-1 )
+        wallLeft.push( widthDisplay*(i-1) )
+    }
+    for(let i =0; i<widthDisplay; i++){ 
+        rowUp.push(i)
+        rowDown.push( ( widthDisplay*(heightDisplay-1) ) +i )
+    }
+
+    
+    const headSnakeIsInBorder = border => border.filter(c => c==snake.indexHead).length
+
+    if(snake.state=='up' && headSnakeIsInBorder(rowUp)){
+        return 'up'
+    }
+    if(snake.state=='down' && headSnakeIsInBorder(rowDown)){
+        return 'down'
+    }
+    if(snake.state=='right' && headSnakeIsInBorder(wallRight)){
+        return 'right'
+    }
+    if(snake.state=='left' && headSnakeIsInBorder(wallLeft)){
+        return 'left'
+    }
+
+    return false //Se não tiver caido em nenhum 'if' de cima
+}
 
 function estructure(){
-    
     for(let i=0; i < pixelsDisplay; i++){
         guy[i] = 0
     }
@@ -175,12 +210,10 @@ function renderDisplay(){
       document.querySelector('#canvas').innerHTML = html 
 }
 
-function restart(){
-
-    for(let i=0; i < pixelsDisplay; i++){
-        guy[i] = 0
-    }
-    start()
+function endGame(){
+    //Função para quando o jogador perder
+    //alert('Você perdeu!')
+    clearInterval(timerLoop)
 }
 
 start()
